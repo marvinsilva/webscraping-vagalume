eacbd8b583e1f92c2c905dff04f621580bf660fc
marvinsilva <45732834+marvinsilva@users.noreply.github.com>
2018-12-09 21:06:44 -0200

    Alterando o host app.run

--- web.py
+++ web.py
@@ -1,17 +1,13 @@
 from flask import Flask
 from flask_restful import Resource, Api
-from crawler import WebScrap
 
 app = Flask(__name__)
 api = Api(app)
-vagalume = WebScrap()
 
 class Student(Resource):
     def get(self, name):
-        top_musicas, alfabet_musicas = vagalume.search(name)
-        print(top_musicas, alfabet_musicas)
-        return {'student': top_musicas}
+        return {'student': name}
 
-api.add_resource(Student, '/top/<string:name>')     # http://127:0.0.1:5000/student/Marcus
+api.add_resource(Student, '/student/<string:name>')     # http://127:0.0.1:5000/student/Marcus
 
-app.run(port=5000)
+app.run(host='0.0.0.0', port=5000)
