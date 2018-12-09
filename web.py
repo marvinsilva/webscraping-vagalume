fdd1c4d9011fea18a18fbf883db15195620e7b04
marvinsilva <45732834+marvinsilva@users.noreply.github.com>
2018-12-09 21:13:38 -0200

    Update web.py

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,4 +11,8 @@ class Student(Resource):
 
 api.add_resource(Student, '/student/<string:name>')     # http://127:0.0.1:5000/student/Marcus
 
+# Bind to PORT if defined, otherwise default to 5000.
+port = int(os.environ.get('PORT', 5000))
+# Tem que ser 0.0.0.0 para rodar no Heroku
+#app.run(host='127.0.0.1', port=port)
 app.run(host='0.0.0.0', port=5000)
