80e093aec401b7145affa2dff515cccf02b86ff3
marvinsilva <45732834+marvinsilva@users.noreply.github.com>
2018-12-09 20:54:46 -0200

    Validação de deploy Heroku
    https://vagalume-web-scrap.herokuapp.com

--- web.py
+++ web.py
@@ -1,26 +1,17 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from crawler import WebScrap
-from unicodedata import normalize
-import re
 
 app = Flask(__name__)
 api = Api(app)
 vagalume = WebScrap()
 
-class TopMusic(Resource):
-    def get(self, artista):
-        artista = remover_acentos_char_especiais(artista)
-        print(artista)
-        top_musicas, alfabet_musicas = vagalume.search(artista)
-        lim_top_musicas = []
-        for seq, top in enumerate(top_musicas):
-            if seq < 15:
-                lim_top_musicas.append(top)
-        return {'Top 15': 'Teste'}
+class Student(Resource):
+    def get(self, name):
+        top_musicas, alfabet_musicas = vagalume.search(name)
+        print(top_musicas, alfabet_musicas)
+        return {'student': top_musicas}
 
-
-api.add_resource(TopMusic, '/top/<string:artista>') # http://127:0.0.1:5000/top/metallica
-
+api.add_resource(Student, '/top/<string:name>')     # http://127:0.0.1:5000/student/Marcus
 
 app.run(port=5000)
