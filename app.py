df275ab6aaf75d328d6a99a6d79e147556538858
marvinsilva <45732834+marvinsilva@users.noreply.github.com>
2018-12-09 11:02:30 -0200

    Adicionando crawler para TopMusics e alfabetMusicList no site vagalume.com

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import getopt
 import sys
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
 
 def main():
     artista = ''
@@ -8,13 +11,15 @@ def main():
     todas = False
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "a:n:m:l:tvh")
+        opts, args = getopt.getopt(sys.argv[1:], "a:n:m:l:tvh",
+                                   ["artista=", "numero=", "musica=", "todas", "help"])
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err)  # will print something like "option -a not recognized"
         sys.exit(2)
 
     for opt, arg in opts:
+
         if opt in ("-a", "--artista"):
             print(f' artista {arg}')
             artista = arg
@@ -31,6 +36,23 @@ def main():
         elif opt in ("-h", "--help"):
             print("Help")
             sys.exit()
+
+    url = f'https://www.vagalume.com.br/{artista}/'
+    print(url)
+    html_doc = urlopen(url).read()
+    soup = BeautifulSoup(html_doc, "html.parser")
+    top_musicas = []
+    alfabet_musicas = []
+
+    for top in soup.find(id="topMusicList").find_all("a", class_="nameMusic"):
+        top_musicas.append(top.get_text())
+
+    for top in soup.find(id="alfabetMusicList").find_all("a", class_="nameMusic"):
+        alfabet_musicas.append(top.get_text())
+
+    print(f'top: {top_musicas}')
+    print(f'alfabetic {alfabet_musicas}')
+
 
 if __name__ == "__main__":
     main()
