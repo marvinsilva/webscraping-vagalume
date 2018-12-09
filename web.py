9556bd8aa5ed41d39af15ee3dac8ddf582e820c8
marvinsilva <45732834+marvinsilva@users.noreply.github.com>
2018-12-09 20:38:29 -0200

    Criação do arquivo 'web.py'
    Arquivo com a funcçao de servir os dados recuperados através de uma API Restful

---
+++ web.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from crawler import WebScrap
+from unicodedata import normalize
+import re
+
+app = Flask(__name__)
+api = Api(app)
+vagalume = WebScrap()
+
+class TopMusic(Resource):
+    def get(self, artista):
+        artista = remover_acentos_char_especiais(artista)
+        print(artista)
+        top_musicas, alfabet_musicas = vagalume.search(artista)
+        lim_top_musicas = []
+        for seq, top in enumerate(top_musicas):
+            if seq < 15:
+                lim_top_musicas.append(top)
+        return {'Top 15': lim_top_musicas}
+
+
+api.add_resource(TopMusic, '/top/<string:artista>') # http://127:0.0.1:5000/top/metallica
+
+
+app.run(port=5000)
