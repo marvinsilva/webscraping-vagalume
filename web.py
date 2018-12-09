12b150085fa7e947d5152b869bc34669efa9ec14
marvinsilva <45732834+marvinsilva@users.noreply.github.com>
2018-12-09 21:25:02 -0200

    Corrigindo port

--- web.py
+++ web.py
@@ -15,4 +15,4 @@ api.add_resource(Student, '/student/<string:name>')     # http://127:0.0.1:5000/
 port = int(os.environ.get('PORT', 5000))
 # Tem que ser 0.0.0.0 para rodar no Heroku
 #app.run(host='127.0.0.1', port=port)
-app.run(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=port)
